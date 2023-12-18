283d9f09d5cbbcdf0d9c5bb73be234ee0d142446
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-12-18 16:01:12 +0500

    Added live page

---
+++ pages/live.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex items-center justify-center">
+    <video
+      id="video"
+      width="80%"
+      height="80%"
+      autoplay
+      muted
+      loop
+      playsinline
+      ref="video"
+      src="http://127.0.0.1:8000/stream_video/"
+    ></video>
+  </div>
+</template>
+
+<style scoped></style>
